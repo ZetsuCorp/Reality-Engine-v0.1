730ce965922efe3575ffc42b16b07d7edf2787cc
Artworqq <keysub.kevin@gmail.com>
2026-08-09 12:46:06 -0400

    Add files via upload

---
+++ Trainer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "BPE.h"
+#include <cstddef>
+#include <string>
+#include <vector>
+
+class BPETrainer {
+public:
+    using Corpus = std::vector<std::string>;
+
+    struct Config {
+        std::size_t target_vocab_size = 32000;
+        std::size_t min_pair_frequency = 2;
+        std::size_t max_merges = 0;
+    };
+
+    BPETrainer();
+    explicit BPETrainer(const Config& config);
+
+    void setConfig(const Config& config);
+    const Config& getConfig() const noexcept;
+    BPE train(const Corpus& corpus) const;
+
+private:
+    Config config_;
+    std::size_t determineMergeCount(std::size_t initialVocabularySize) const;
+};
